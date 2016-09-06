a366b7a8fd5329e2e3e2d7daf290e635b2097273
root <root@ip-172-31-19-209.eu-west-1.compute.internal>
2016-09-06 15:16:45 +0000

    Added data

--- api/utils/convertImage.js
+++ api/utils/convertImage.js
@@ -1,31 +1,44 @@
 import  im from 'imagemagick';
 const sizes = require('../data/imageSizes.json');
+const async = require('async'); 
 
 const destinationPath =  './static/images/';
 
 export default function convertImage(sourcePath, filename) {
   return new Promise((resolve, reject) => {
     if(filename && sourcePath){
-      sizes.map((size, index) => {
-        const newPath = destinationPath + size.join('x') + '/' + filename;
-        const config = {
-          srcPath: sourcePath,
-          dstPath: newPath,
-          quality:0.8,
-          width: size[0],
-          height: size[1]
-        };
-        im.crop(config,
-        function(err, stdout, stderr){
-          if (err) reject(err);
-          console.log('resized '+ sourcePath + ' to fit within' + size.join('x'));
-          resolve(filename);
-        });
+      let actions = [];
+      const finish =  function (next){
+        resolve(filename);
+        next();
+      }
 
+      sizes.map((size, index) => {
+        actions.push(function(next){
+          const newPath = destinationPath + size.join('x') + '/' + filename;
+          const config = {
+            srcPath: sourcePath,
+            dstPath: newPath,
+            quality:0.8,
+            width: size[0],
+            height: size[1]
+          };
+          im.crop(config,
+          function(err, stdout, stderr){
+            if (err) reject(err);
+            console.log('resized '+ sourcePath + ' to fit within' + size.join('x'));
+            next();
+          });
+        });
       });
+
+      actions.push(finish);
+      async.series(actions);
+
     }
     else{
       reject('path and filename must be set');
     }
   });
 }
+
