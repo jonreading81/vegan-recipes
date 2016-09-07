112268d68a2859ca55de3dafccd8a51b81b9cc6c
Jonathan Reading <jon.reading.email@gmail.com>
2016-09-07 16:26:26 +0100

    Custom scroll condition to not scroll if same page

--- src/utils/customScroll.js
+++ src/utils/customScroll.js
@@ -1,9 +1,10 @@
 export default function(prevLocation, location) {
-  if (prevLocation &&
-    prevLocation.pathname.search('recipe/list') === 1 &&
-    location.pathname.search('recipe/list') === 1
-    ) {
-    return false;
+  if (prevLocation) {
+    const samePage = (prevLocation.pathname === location.pathname);
+    const recipeListAction = (prevLocation.pathname.search('recipe/list') === 1 && location.pathname.search('recipe/list') === 1);
+    if (samePage || recipeListAction) {
+      return false;
+    }
   }
   return true;
 }
