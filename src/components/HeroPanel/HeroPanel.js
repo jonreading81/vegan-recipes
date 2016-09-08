cec00fe6996e8771c8302a3adb3b92640c9089e3
amyvarga <amymvarga@yahoo.co.uk>
2016-09-08 15:28:41 +0100

    Adds birdsong.mp3 to home page.  Changes text on home page

--- src/components/HeroPanel/HeroPanel.js
+++ src/components/HeroPanel/HeroPanel.js
@@ -14,11 +14,12 @@ export default class HeroPanel extends Component {
     isEmpty: PropTypes.bool,
     hasBreadcrumb: PropTypes.bool,
     subTitle: PropTypes.string,
-    image: PropTypes.string.isRequired
+    image: PropTypes.string.isRequired,
+    audio: PropTypes.string
   }
   render() {
     require('./HeroPanel.scss');
-    const {title, subTitle, image, isEmpty, children, hasBreadcrumb} = this.props;
+    const {title, subTitle, image, isEmpty, children, hasBreadcrumb, audio} = this.props;
     const type = get( this.props, 'type', 'site-heading');
     let className = 'hero-panel';
     let style = this.props.style;
@@ -38,8 +39,16 @@ export default class HeroPanel extends Component {
             </div>
             <div className="hero-panel-type">
               <div className="hero-panel-type-lining">
+                <If condition={!isUndefined(audio)}>
+                  <div className="audio-wrapper">
+                    <audio controls autoPlay>
+                      <source src={audio} type="audio/mp3" />
+                    </audio>
+                  </div>
+                </If>
                 <div className={type}>
                     <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+
                       <If condition={!isUndefined(title)}>
                         <h1>{title}</h1>
                         <hr className="small" />
