621265319056f9a942f621b2be110f21c7c67402
Jonathan Reading <jon.reading.email@gmail.com>
2016-09-26 14:16:40 +0100

    Fixed Merge issues

--- src/client.js
+++ src/client.js
@@ -6,7 +6,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import createStore from './redux/create';
 import ApiClient from './helpers/ApiClient';
-// import io from 'socket.io-client';
+import io from 'socket.io-client';
 import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
@@ -23,7 +23,7 @@ const dest = document.getElementById('content');
 const store = createStore(_browserHistory, client, window.__data);
 const history = syncHistoryWithStore(_browserHistory, store);
 
-{function initSocket() {
+function initSocket() {
   const socket = io('', {path: '/ws'});
   socket.on('news', (data) => {
     console.log(data);
@@ -34,7 +34,7 @@ const history = syncHistoryWithStore(_browserHistory, store);
   });
 
   return socket;
-}}
+}
 
 function logPageView() {
   ReactGA.set({ page: window.location.pathname });
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -38,35 +38,18 @@ export default class Home extends Component {
     return (
       <div>
         <Helmet title="Home"/>
-<<<<<<< HEAD
          <If condition={isFetching}>
             <Loading />
           </If>
           <If condition={!isFetching}>
-            <HeroPanel image={article.getImage()} audio={config.app.birdsong} title={article.getTitle()}style="image-focus-bottom">
+            <HeroPanel image={article.getImage()} title={article.getTitle()}style="image-focus-bottom">
             {subTextComponent}
             </HeroPanel>
-            <div className="container">
-              <div className="body-copy">{contentComponent}</div>
-=======
-          <HeroPanel image="forest.jpeg" title={config.app.title} style="image-focus-bottom">
-          <span className="subheading">The forest can be a metaphor for a spirit of freedom attainable within any lifestyle <cite><a href={dharmaocean}>DharmaOcean.org</a></cite></span>
-          </HeroPanel>
             <audio controls>
               <source src={config.app.birdsong} type="audio/mp3" />
             </audio>
-          <div className="container">
-            <div className="body-copy">
-              <p className="body-copy-first important">
-              What does the forest bring to mind when you quiet your mind and allow your imaginations to roam freely?</p>
-              <p>
-              Do you hear the whisper of swaying trees rising above you?  Do you see dancing sun beams gleaming on leaf covered tracks?  The ever present knowing that the forest provides a sustainable home for all its inhabitants.  The glimpse of rabbits, mice, hedgehogs and moles who make their homes amongst the interconnected web of roots underfoot, the giggling gurgling of a sparkling stream, the chirping and singing of birds and with the setting sun a closing blackness whose only solace are silver beams of moonshine.
-              </p>
-              <blockquote>
-              The forests are calling us to return to our authentic selves.  It is not so much giving up on our modern culture but more that we now take the time to <strong>listen</strong> to our inner selves, to <strong>feel</strong> rather than think, to <strong>be</strong> rather than do.  Then we hear our hearts calling for our freedom, a freedom that we are all seeking.
-              </blockquote>
-              </div>
->>>>>>> 7da312eb0aa5384d21e8e12758cfde1974552eb0
+            <div className="container">
+              <div className="body-copy">{contentComponent}</div>
             </div>
           </If>
         </div>
