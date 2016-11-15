2cc1d6eac3a135765e46dabb6fda4a4e004156ab
Jonathan Reading <jon.reading.email@gmail.com>
2016-11-15 21:49:20 +0000

    Added admin check to admin actions

--- src/components/ItemsGrid/ItemsGrid.js
+++ src/components/ItemsGrid/ItemsGrid.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import {Row, Col, Button, ButtonToolbar } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
-import {ImagePromoUnit} from 'components';
+import {ImagePromoUnit, AdminUser} from 'components';
 
 export default class ItemsGrid extends Component {
 
@@ -20,16 +20,18 @@ export default class ItemsGrid extends Component {
         <For each="item" index="index" of={ this.props.items}>
           <Col xs={12} sm={6} md={4} className={styles.promo}>
             <ImagePromoUnit title={item.title} URL={item.URL} description={item.description} image={item.image} icon={item.icon} color={item.color}/>
-            <If condition={hasAdminActions}>
-              <ButtonToolbar className={styles.adminToolbar}>
-              <LinkContainer to={item.updateURL}>
-                <Button bsStyle="primary" bsSize="large" >Update</Button>
-              </LinkContainer>
-              <LinkContainer to={item.deleteURL}>
-                <Button bsStyle="primary" bsSize="large" >Delete</Button>
-              </LinkContainer>
-              </ButtonToolbar>
-            </If>
+            <AdminUser>
+              <If condition={hasAdminActions}>
+                <ButtonToolbar className={styles.adminToolbar}>
+                <LinkContainer to={item.updateURL}>
+                  <Button bsStyle="primary" bsSize="large" >Update</Button>
+                </LinkContainer>
+                <LinkContainer to={item.deleteURL}>
+                  <Button bsStyle="primary" bsSize="large" >Delete</Button>
+                </LinkContainer>
+                </ButtonToolbar>
+              </If>
+            </AdminUser>
           </Col>
           {(index + 1) % 2 === 0 ? <div className="clearfix visible-sm-block"></div> : null }
           {(index + 1) % 3 === 0 ? <div className="clearfix visible-md-block visible-lg-block"></div> : null }
